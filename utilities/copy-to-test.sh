27df2805b6eeb768bb92775b8b75bd0e5db0045e
Sigfrid Lundberg <sigfrid@sigfrid-lundberg.se>
2021-07-16 17:45:50 +0200

    got the correct --exclude option

--- utilities/copy-to-test.sh
+++ utilities/copy-to-test.sh
@@ -4,5 +4,5 @@ export DESTINATION=/home/sigge/Dropbox/fsdata
 
 # mkdir -p "$DESTINATION"
 
-tar --xattrs-exclude='^\.git'  -cvf - . | (cd "$DESTINATION" ; tar xvf - )
+tar --exclude='\.git'  -cvf - . | (cd "$DESTINATION" ; tar xvf - )
 
